12879ee7a8a14bd77939dc587bb13ef745045280
hocky <hocky.yudhiono@gmail.com>
2022-01-10 01:46:37 +0700

    Fix head

--- pages/index.js
+++ pages/index.js
@@ -19,119 +19,130 @@ import LanguageItem from "../components/LanguageItem";
 import SectionContainer from "../components/SectionContainer";
 import AchievementItem from "../components/AchievementItem";
 import ExperienceItem from "../components/ExperienceItem";
+import Head from 'next/head'
 
 export default function Home() {
   console.log(data)
   return (
-    <Container maxW='container.lg' className={'mt-5 justify-center'}>
-      <Flex className={'justify-between'}>
-        <Flex className={'flex-col justify-evenly pl-3'}>
-          <div className={'text-lg'}>
-            <h1 className={'text-5xl mb-3'}>
-              {data.info.firstName}
-              <span className={'font-bold'}>
+    <div>
+      <Head>
+        <meta charset="UTF-8"/>
+        <title>Hocky Yudhiono's CV</title>
+        <meta name="keywords" content="CV, Resume, Hocky, Yudhiono, About"/>
+        <meta name="author" content="Hocky"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+      <Container maxW='container.lg' className={'mt-5 justify-center'}>
+        <Flex className={'justify-between'}>
+          <Flex className={'flex-col justify-evenly pl-3'}>
+            <div className={'text-lg'}>
+              <h1 className={'text-5xl mb-3'}>
+                {data.info.firstName}
+                <span className={'font-bold'}>
               {data.info.boldedName}
             </span>
-              {data.info.lastName}
-            </h1>
-            <h1 className={'text-3xl'}>
-              {data.info.caption}
-            </h1>
-          </div>
-        </Flex>
-        <Flex className={'flex-col pr-5 text-lg'}>
-          <HeaderItem>
-            <IconHeaderItem icon={faGlobeAsia}></IconHeaderItem>
-            <Link href={data.info.website} isExternal>
-              {data.info.website} <ExternalLinkIcon mx='1px'/>
-            </Link>
-          </HeaderItem>
-          <HeaderItem>
-            <IconHeaderItem icon={faMapMarked}></IconHeaderItem>
-            <div>
-              {data.info.location}
+                {data.info.lastName}
+              </h1>
+              <h1 className={'text-3xl'}>
+                {data.info.caption}
+              </h1>
             </div>
-          </HeaderItem>
-          <HeaderItem>
-            <IconHeaderItem icon={faPhone}></IconHeaderItem>
-            <div>
-              <Link href={'tel:' + data.info.phone.replace(/(\s|-)/g, '')} isExternal>
-                {data.info.phone}
+          </Flex>
+          <Flex className={'flex-col pr-5 text-lg'}>
+            <HeaderItem>
+              <IconHeaderItem icon={faGlobeAsia}></IconHeaderItem>
+              <Link href={data.info.website} isExternal>
+                {data.info.website} <ExternalLinkIcon mx='1px'/>
               </Link>
-            </div>
-          </HeaderItem>
-          <HeaderItem>
-            <IconHeaderItem icon={faLinkedin}></IconHeaderItem>
-            <Link href={'https://linkedin.com/in/' + data.info.linkedin} isExternal>
-              {data.info.linkedin}
-            </Link>
-          </HeaderItem>
-          <HeaderItem>
-            <IconHeaderItem icon={faEnvelope}></IconHeaderItem>
-            <Link href={'mailto:' + data.info.email} isExternal>
-              {data.info.email}
-            </Link>
-          </HeaderItem>
+            </HeaderItem>
+            <HeaderItem>
+              <IconHeaderItem icon={faMapMarked}></IconHeaderItem>
+              <div>
+                {data.info.location}
+              </div>
+            </HeaderItem>
+            <HeaderItem>
+              <IconHeaderItem icon={faPhone}></IconHeaderItem>
+              <div>
+                <Link href={'tel:' + data.info.phone.replace(/(\s|-)/g, '')} isExternal>
+                  {data.info.phone}
+                </Link>
+              </div>
+            </HeaderItem>
+            <HeaderItem>
+              <IconHeaderItem icon={faLinkedin}></IconHeaderItem>
+              <Link href={'https://linkedin.com/in/' + data.info.linkedin} isExternal>
+                {data.info.linkedin}
+              </Link>
+            </HeaderItem>
+            <HeaderItem>
+              <IconHeaderItem icon={faEnvelope}></IconHeaderItem>
+              <Link href={'mailto:' + data.info.email} isExternal>
+                {data.info.email}
+              </Link>
+            </HeaderItem>
+          </Flex>
         </Flex>
-      </Flex>
-      <hr className={'border-0 bg-black text-black h-1 mt-2 mb-2'}/>
-      <Flex className={'flex-col lg:flex-row'}>
-        <Flex className={'p-2 flex-col'}>
-          <SectionContainer>
-            <h2 className={'text-2xl'}>Education</h2>
-            {
-              data.education.map(educationItem => (<EducationItem educationItem={educationItem}/>))
-            }
-          </SectionContainer>
+        <hr className={'border-0 bg-black text-black h-1 mt-2 mb-2'}/>
+        <Flex className={'flex-col lg:flex-row'}>
+          <Flex className={'p-2 flex-col'}>
+            <SectionContainer>
+              <h2 className={'text-2xl'}>Education</h2>
+              {
+                data.education.map(educationItem => (<EducationItem educationItem={educationItem}/>))
+              }
+            </SectionContainer>
 
-          <SectionContainer>
-            <h2 className={'text-2xl'}>Employment History</h2>
-            {
-              data.employment.map(employmentItem => (<EmploymentItem employmentItem={employmentItem}/>))
-            }
-          </SectionContainer>
+            <SectionContainer>
+              <h2 className={'text-2xl'}>Employment History</h2>
+              {
+                data.employment.map(employmentItem => (<EmploymentItem employmentItem={employmentItem}/>))
+              }
+            </SectionContainer>
+          </Flex>
+          <Flex className={'p-2 flex-col'}>
+
+            <SectionContainer>
+              <h2 className={'text-2xl'}>Professional Skills</h2>
+              <UnorderedList>
+                {
+                  data.skills.map(skillItem => (<SkillItem skillItem={skillItem}/>))
+                }
+              </UnorderedList>
+            </SectionContainer>
+
+            <SectionContainer>
+              <h2 className={'text-2xl'}>Languages</h2>
+              <UnorderedList>
+                {
+                  data.languages.map(languageItem => (<LanguageItem languageItem={languageItem}/>))
+                }
+              </UnorderedList>
+            </SectionContainer>
+
+
+            <SectionContainer>
+              <h2 className={'text-2xl'}>Achievements</h2>
+              <UnorderedList>
+                {
+                  data.achievements.map(achievementItem => (<AchievementItem achievementItem={achievementItem}/>))
+                }
+              </UnorderedList>
+            </SectionContainer>
+
+
+            <SectionContainer>
+              <h2 className={'text-2xl'}>Relevant Experiences</h2>
+              <UnorderedList>
+                {
+                  data.others.map(experienceItem => (<ExperienceItem experienceItem={experienceItem}/>))
+                }
+              </UnorderedList>
+            </SectionContainer>
+          </Flex>
         </Flex>
-        <Flex className={'p-2 flex-col'}>
+      </Container>
+    </div>
 
-          <SectionContainer>
-            <h2 className={'text-2xl'}>Professional Skills</h2>
-            <UnorderedList>
-              {
-                data.skills.map(skillItem => (<SkillItem skillItem={skillItem}/>))
-              }
-            </UnorderedList>
-          </SectionContainer>
-
-          <SectionContainer>
-            <h2 className={'text-2xl'}>Languages</h2>
-            <UnorderedList>
-              {
-                data.languages.map(languageItem => (<LanguageItem languageItem={languageItem}/>))
-              }
-            </UnorderedList>
-          </SectionContainer>
-
-
-          <SectionContainer>
-            <h2 className={'text-2xl'}>Achievements</h2>
-            <UnorderedList>
-              {
-                data.achievements.map(achievementItem => (<AchievementItem achievementItem={achievementItem}/>))
-              }
-            </UnorderedList>
-          </SectionContainer>
-
-
-          <SectionContainer>
-            <h2 className={'text-2xl'}>Relevant Experiences</h2>
-            <UnorderedList>
-              {
-                data.others.map(experienceItem => (<ExperienceItem experienceItem={experienceItem}/>))
-              }
-            </UnorderedList>
-          </SectionContainer>
-        </Flex>
-      </Flex>
-    </Container>
   )
 }
